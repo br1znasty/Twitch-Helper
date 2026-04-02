7c3232aef187e2a0d1a11833e1db203b70879eec
mashawarface <sony.sky.16@gmail.com>
2026-04-02 19:57:17 +0700

    add: token for api

--- twitch-helper-docker/db/init/init.sql
+++ twitch-helper-docker/db/init/init.sql
@@ -2,5 +2,9 @@ CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     username VARCHAR(100) NOT NULL UNIQUE,
     email VARCHAR(255) NOT NULL UNIQUE,
-    password_hash VARCHAR(255) NOT NULL
+    password_hash VARCHAR(255) NOT NULL,
+    client_id VARCHAR(255),
+    client_secret VARCHAR(255),
+    access_token VARCHAR(255),
+    expired_at BIGINT
 )
