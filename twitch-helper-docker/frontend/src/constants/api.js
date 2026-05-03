7d9499c284a0d38fb37899f4c43d99fc53f8bc8d
mashawarface <sony.sky.16@gmail.com>
2026-05-03 19:22:48 +0700

    add: vite proxy

--- twitch-helper-docker/frontend/src/constants/api.js
+++ twitch-helper-docker/frontend/src/constants/api.js
@@ -1,3 +1,3 @@
-export const AUTH_API_URL = "http://localhost:8081";
-export const USER_API_URL = "http://localhost:8082";
-export const TWITCH_API_URL = "http://localhost:8083";
+export const AUTH_API_URL = "";
+export const USER_API_URL = "";
+export const TWITCH_API_URL = "";
--- twitch-helper-docker/frontend/vite.config.js
+++ twitch-helper-docker/frontend/vite.config.js
@@ -3,6 +3,21 @@ import { defineConfig } from "vite";
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    port: 3000
-  }
+    port: 3000,
+    allowedHosts: true,
+    proxy: {
+      "/api/auth": {
+        target: "http://auth-service:8080",
+        changeOrigin: true,
+      },
+      "/api/users": {
+        target: "http://user-service:8080",
+        changeOrigin: true,
+      },
+      "/api/twitch": {
+        target: "http://twitch-service:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
